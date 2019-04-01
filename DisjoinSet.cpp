d9d332497a607d5d8787c29a3b247454c4e1747d
Chess <gyc880323@gmail.com>
2019-04-01 21:16:36 +0800

    Update DisjoinSet.cpp

--- DisjoinSet.cpp
+++ DisjoinSet.cpp
@@ -10,13 +10,16 @@ void init(int n) // give a initial length
 
 int find(int x)
 { // find the father point
-	return arr[x] < 0 ? x : (arr[x] = find(arr[x]));
+	return arr[x] < 0 ? x : (arr[x] = find(arr[x])); // update every child to the other father
 }
 
 void Union(int x, int y)
 {
 	x = find(x);
 	y = find(y);
+	
+	if(x == y)
+		return;
 
 	if(arr[x] <= arr[y])
 	{
