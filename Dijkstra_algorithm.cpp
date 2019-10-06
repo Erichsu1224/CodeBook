26b25e092290a00173d7c132d481f6ada10ffc00
Eric <40513062+Erichsu1224@users.noreply.github.com>
2019-10-06 17:59:01 +0800

    Upload dijkstra

--- Dijkstra_algorithm.cpp
+++ Dijkstra_algorithm.cpp
@@ -1,97 +1,42 @@
-//dijkstra
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define maxn 51415
 
-struct Edge
-{
-	int from, to, dist;
+#define MP make_pair
+#define PII pair<int, int>
+#define maxn 50000+5
 
-	Edge(int _from, int _to, int _dist)
-    {
-        from = _from;
-        to = _to;
-        dist = _dist;
-    }
-};
- 
-struct Item
-{
-    int node;
-    int dist;
- 
-    Item(int _node, int _dist)
-    {
-        node = _node;
-        dist = _dist;
-    }
- 
-    bool operator <(const Item& rs) const
-    {
-        return dist > rs.dist;
-    }
-};
+int dis[maxn]; // 預設都是 INF
+vector<PII > e[maxn]; // (連到的點， 邊的距離)
 
-int n, m;
-vector <Edge> edges;
-vector<int> G[maxn]; 
-bool visit[maxn];
-int start, node;
- 
-int main(void)
-{
-    while(cin >> n >> m)
-    {
-    	//init
-        priority_queue <Item> dij;
-        edges.clear();
-        for(int i = 0; i <= n; i++)
+void dijk(int cur) // dijk(起點)
+{ 
+  int d;
+  priority_queue<PII,vector<PII>,greater<PII>> q; // 放 (距離, 點編號)，每次會拿距離最小的點出來
+  q.push( MP(0, cur) );
+    
+  while (!q.empty()) 
+  {
+    tie(d, cur) = q.top();
+    q.pop();
+    if (dis[cur] != 1e9) 
+        continue; // 如果之前就拜訪過，無視
+
+    dis[cur] = d;
+
+    for (auto i: e[cur])
+        if (dis[i.first] == 1e9) 
         {
-            G[i].clear();
+            q.push( MP(d+i.second, i.first) );
         }
-        memset(visit, false, sizeof(visit));
-
- 
-        for(int i = 0; i < m; i++)
-        {
-            int a, b, c;
-            scanf("%d %d %d", &a, &b, &c);
-            edges.push_back(Edge(a, b, c));
-            G[a].push_back(i);
-        }
-
-        // start vertex
-        start = 1;
-        node = 0;
-        
-        dij.push(Item(start, 0));
-        Item hold = Item(0, 0);
-        while(!dij.empty())
-        {
-            hold = dij.top(); 
-            dij.pop();
-            cout << hold.node << ' ' << hold.dist << endl;
-
-            if(visit[hold.node] == true)
-                continue;
-
-            visit[hold.node] = true;
-
-            node = hold.node;
-            if(node == n)
-            {
-                break;
-            }
- 
-            for(int i = 0; i < G[node].size(); i++)
-            {
-               dij.push(Item(edges[G[node][i]].to, hold.dist+edges[G[node][i]].dist));
-            }
-        }
-
-        if(node != n) printf("-1\n");
-        if(node == n)  printf("%d\n", hold.dist);
     }
-    return 0;
+}
+
+void init(void)
+{
+    fill(dis, dis+maxn, 1e9);
+
+    for(int i = 0; i < maxn; i++)
+    {
+        e[i].clear();
+    }
 }
