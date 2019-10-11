7ba056451a3474642712ad48c2f3cf9f4a11e851
Eric <40513062+Erichsu1224@users.noreply.github.com>
2019-10-11 17:16:09 +0800

    Add maximum distanse between two vertex on tree

--- template.cpp
+++ template.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 #define ll long long
 #define PB push_back
-#define PP pair<int, int>
+#define PII pair<int, int>
+#define MP make_pair
 #define IOS ios_base::sync_with_stdio(false); cin.tie(0)
 #define all(x) x.begin(), x.end()
 #define REP(x, y, z) for(int x = y; x <= z; x++)
@@ -18,12 +19,6 @@ using namespace std;
 int main(void)
 {
 	IOS;
-	
-	#ifdef DEBUG
-	freopen("in.in", "r", stdin);
-	freopen("out.out", "w", stdout);
-	#endif
-
 
 
 	return 0;
