14e0da04d02608fda993dd5cfe63aa36973d4849
Eric <40513062+Erichsu1224@users.noreply.github.com>
2019-10-11 16:48:24 +0800

    Add template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h/h>
+using namespace std;
+
+#define ll long long
+#define PB push_back
+#define PP pair<int, int>
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0)
+#define all(x) x.begin(), x.end()
+#define REP(x, y, z) for(int x = y; x <= z; x++)
+#define maxn
+
+//structure
+
+//declaration
+
+//functions
+
+int main(void)
+{
+	IOS;
+	
+	#ifdef DEBUG
+	freopen("in.in", "r", stdin);
+	freopen("out.out", "w", stdout);
+	#endif
+
+
+
+	return 0;
+}
