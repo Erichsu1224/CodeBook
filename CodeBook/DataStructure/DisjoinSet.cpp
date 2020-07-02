34f248d5c03ceb28e254318e035ffe3f95bbef3d
SunTalk <infernal56727@gmail.com>
2020-07-02 13:14:31 +0800

    update disjoinset

--- CodeBook/DataStructure/DisjoinSet.cpp
+++ CodeBook/DataStructure/DisjoinSet.cpp
@@ -1,34 +1,27 @@
 #define SIZE 10000
 
-int arr[SIZE];
-
-void init(int n) // give a initial length
-{
-	for(int i=0; i<n; i++)
-		arr[i] = -1;
-}
-
-int find(int x)
-{ // find the father point
-	return arr[x] < 0 ? x : (arr[x] = find(arr[x])); // update every child to the other father
-}
-
-void Union(int x, int y)
-{
-	x = find(x);
-	y = find(y);
+struct disjoint{
 	
-	if(x == y)
-		return;
+	int arr[SIZE+10];
+	
+	void init(){
+		memset(arr,-1,sizeof(arr));
+	}
 
-	if(arr[x] <= arr[y])
-	{
-		arr[x] += arr[y];
-		arr[y] = x;
+	int find(int k){
+		return arr[k]<0 ? k:(arr[k]=find(arr[k]));
 	}
-	else
-	{
-		arr[y] += arr[x];
-		arr[x] = y;
+
+	void uni(int a, int b){
+		a = find(a);
+		b = find(b);
+		arr[a] += arr[b];
+		arr[b] = a;
 	}
-}
+
+	int siz(int k){
+		return (-arr[find(k)])
+	}
+
+}D;
+
