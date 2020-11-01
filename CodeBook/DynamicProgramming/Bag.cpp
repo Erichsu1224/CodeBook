41bfdfdc941d10a42c2ca9a6bfb0308efbc216c8
SunTalk <infernal56727@gmail.com>
2020-11-01 13:58:45 +0800

    fix dp/bag delete dp/coin

--- CodeBook/DynamicProgramming/Bag.cpp
+++ CodeBook/DynamicProgramming/Bag.cpp
@@ -36,16 +36,16 @@ int limit[N]; // 物品上限
 void Multiple(){
 	for (int i = 0; i < N; i++){
 		int tmp = 1;
-		while (tmp <= weight[i]){
+		while (tmp <= limit[i]){
 			for (int j = 0; j < W; j++)
 				if (j >= weight[i] * tmp)
 					bag[j][1] = max(bag[j - weight[i] * tmp][0] + value[i] * tmp, bag[j][0]);
 			for (int j = 0; j < W; j++)
 				bag[j][0] = bag[j][1];
-			weight[i] = weight[i] - tmp;
+			limit[i] = limit[i] - tmp;
 			tmp = tmp * 2;
 		} // end while
-		if (weight[i] > 0){
+		if (limit[i] > 0){
 			for (int j = 0; j < W; j++)
 				if (j >= weight[i] * tmp)
 					bag[j][1] = max(bag[j - weight[i] * tmp][0] + value[i] * tmp, bag[j][0]);
